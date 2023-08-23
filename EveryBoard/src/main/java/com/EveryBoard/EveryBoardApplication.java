58d2eb323e15c04c186da8e4baf5c38077f62c4a
praysw <praysw@naver.com>
2023-08-24 00:03:33 +0900

    2023.08.23 package 구조 변경

--- EveryBoard/src/main/java/com/EveryBoard/EveryBoardApplication.java
+++ EveryBoard/src/main/java/com/EveryBoard/EveryBoardApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EveryBoardApplication {
-
+	
 	public static void main(String[] args) {
+		System.out.println("Spring Boot Start!!!");
+		System.out.println("EveryBoardApplication.java");
 		SpringApplication.run(EveryBoardApplication.class, args);
 	}
 
--- EveryBoard/src/main/java/com/EveryBoard/EveryBoardMainController.java
+++ EveryBoard/src/main/java/com/EveryBoard/EveryBoardMainController.java
@@ -1,24 +1,24 @@
-package com.EveryBoardControllers;
+package com.EveryBoard;
 
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EveryBoardMainController {
 	
-	@ResponseBody
+	@RequestMapping("/main.do")
+	public String Main() {
+		System.out.println("Main Controller  실행됨");
+		System.out.println("/main.do");
+		return "main";
+	}
+	
+	/*
 	@RequestMapping("/error") 
 	public String error() {
 		System.out.println("아이고 에러났슈");
 		return "아이고 에러났슈";
 		
-	}
-	
-	@RequestMapping("/main.do") 
-	public void main() {
-		System.out.println("main.do 실행");
-		
-	}
+	}*/
 	
 }
